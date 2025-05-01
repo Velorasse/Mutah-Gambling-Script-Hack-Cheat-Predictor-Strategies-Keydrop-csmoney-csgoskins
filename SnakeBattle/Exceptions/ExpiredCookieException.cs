8d872a6cf9e196958fa2eb5c8ba643ed4309c17a
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:21 +0300

    ExpiredCookieException.cs

---
+++ SnakeBattle/Exceptions/ExpiredCookieException.cs
@@ -0,0 +1,10 @@
+﻿namespace KeyDropGiveawayBot.Exceptions;
+
+public class ExpiredCookieException : Exception
+{
+    public ExpiredCookieException(
+        string message = "Your cookie has expired.") :
+        base(message)
+    {
+    }
+}
