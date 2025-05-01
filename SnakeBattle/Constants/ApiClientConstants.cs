fe6674608c094c9edde9cda89b9919c6ad769704
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:18 +0300

    ApiClientConstants.cs

---
+++ SnakeBattle/Constants/ApiClientConstants.cs
@@ -0,0 +1,13 @@
+namespace KeyDropGiveawayBot.Constants;
+
+public static class ApiClientConstants
+{
+    public const char QuestionMark = '?';
+    public const char Equal = '=';
+    public const char And = '&';
+    public const char Comma = ',';
+    public const char Quota = '"';
+    public const string JsonMediaType = "application/json";
+    public const string JsonPatchMediaType = "application/json-patch+json";
+    public const string Slash = "/";
+}
